e60fb5672757ef7af9694988bafd43e8cf634dac
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-03 22:00:53 +0700

    dinamis css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('build/assets/app-d0xjzRrD.css') }}">
-    <script src="{{ asset('build/assets/app-DspuE8pW.js') }}" defer></script>
+    @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     <title>{{ $title }}</title>
 </head>
 
 <body class="h-full">
-    <div class="min-h-full">
-        <x-navbar></x-navbar>
+<div class="min-h-full">
+    <x-navbar></x-navbar>
+  
+    <x-header>{{ $title }}</x-header>
 
-        <x-header>{{ $title }}</x-header>
-
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                {{ $slot }}
-            </div>
-        </main>
-    </div>
+    <main>
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        {{ $slot }}
+      </div>
+    </main>
+  </div>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 
-<x-footer></x-footer>
+  <x-footer></x-footer>
