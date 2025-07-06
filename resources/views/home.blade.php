f852ebd7c2013df5f2c194506d4122d18c78a9da
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-06 10:51:22 +0700

    adjusments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,18 +34,17 @@
 
     <section class="mt-8 max-w-3xl mx-auto px-4 text-center">
         <h2 class="text-2xl font-bold text-gray-800 mb-6">Tech Stack</h2>
-        <div class="flex flex-wrap justify-center text-3xl text-gray-700">
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-php"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-laravel"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-square-js"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-react"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-css3-alt"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-node-js"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-brands fa-java"></i></div>
-            <div class="w-1/4 md:w-auto flex justify-center mb-4"><i class="fa-solid fa-database"></i></div>
+        <div class="flex justify-center flex-wrap gap-5 text-3xl text-gray-700">
+            <i class="fa-brands fa-php"></i>
+            <i class="fa-brands fa-laravel"></i>
+            <i class="fa-brands fa-square-js"></i>
+            <i class="fa-brands fa-react"></i>
+            <i class="fa-brands fa-css3-alt"></i>
+            <i class="fa-brands fa-node-js"></i>
+            <i class="fa-brands fa-java"></i>
+            <i class="fa-solid fa-database"></i>
         </div>
     </section>
-
 
     <section class="mt-8 max-w-xl mx-auto text-center text-gray-600 italic">
         "First, solve the problem. Then, write the code."
