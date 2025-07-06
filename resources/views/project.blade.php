28e8764e400b34b32eac3538a86dabe4b1996732
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-06 11:41:21 +0700

    adjusments

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -38,7 +38,7 @@
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
                     @foreach ($project->images as $image)
                         <div
-                            class="h-auto max-h-96 bg-gray-100 flex items-center justify-center overflow-hidden rounded-2xl">
+                            class="h-auto max-h-96 bg-gray-100 flex items-center justify-center overflow-hidden rounded-2xl shadow">
                             <img src="{{ asset($image->image) }}" alt="Landing Page {{ $project->title }}"
                                 class="object-contain max-h-96 w-full">
                         </div>
