51cee7efa5da9894a3c3642e37cccdbe0239ccc1
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-06 11:03:08 +0700

    adjusments

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -13,8 +13,8 @@
                 <p class="text-sm text-gray-500 mt-1">{{ $project->created_at->diffForHumans() }}</p>
             </div>
 
-            @if ($project->technologies->isNotEmpty())
-                <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+            <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                @if ($project->technologies->isNotEmpty())
                     <div class="flex flex-col mb-4 sm:mb-0">
                         <span class="text-lg font-semibold text-gray-800 mb-2">Tech Used:</span>
                         <div class="flex flex-wrap gap-4 text-2xl text-gray-700">
@@ -23,29 +23,28 @@
                             @endforeach
                         </div>
                     </div>
+                @endif
+
+                <div class="flex items-center gap-6 text-sm">
+                    <a href="{{ $project->github }}" target="_blank"
+                        class="flex items-center gap-2 text-gray-700 hover:text-black transition">
+                        <i class="fa-brands fa-github text-lg"></i> Source Code
+                    </a>
                 </div>
-            @endif
-
-            <div class="flex items-center gap-6 text-sm">
-                <a href="{{ $project->github }}" target="_blank"
-                    class="flex items-center gap-2 text-gray-700 hover:text-black transition">
-                    <i class="fa-brands fa-github text-lg"></i> Source Code
-                </a>
             </div>
-        </div>
 
-        <div class="mt-10">
-            <h3 class="text-xl font-semibold text-gray-700 mb-4">Pages</h3>
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                @foreach ($project->images as $image)
-                    <div
-                        class="w-full h-auto max-h-96 bg-white flex items-center justify-center overflow-hidden rounded-2xl shadow">
-                        <img src="{{ asset($image->image) }}" alt="Landing Page {{ $project->title }}"
-                            class="object-contain max-h-96 w-full">
-                    </div>
-                @endforeach
+            <div class="mt-10">
+                <h3 class="text-xl font-semibold text-gray-700 mb-4">Pages</h3>
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                    @foreach ($project->images as $image)
+                        <div
+                            class="w-full h-auto max-h-96 bg-white flex items-center justify-center overflow-hidden rounded-2xl shadow">
+                            <img src="{{ asset($image->image) }}" alt="Landing Page {{ $project->title }}"
+                                class="object-contain max-h-96 w-full">
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        </div>
         </div>
     </section>
 </x-layout>
