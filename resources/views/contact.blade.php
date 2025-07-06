7d01fa6ed03ce8a9d279eee66a3631d564b30be4
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-06 12:26:47 +0700

    adjusments

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,53 +1,47 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    
+
     <div class="bg-gray py-24 sm:py-32 mt-10 flex flex-col items-center">
         <div class="mx-auto max-w-7xl px-6 lg:px-8 text-center">
             <h2 class="text-3xl font-semibold text-gray-900">Contact Me!</h2>
             <div class="mt-20 flex flex-wrap justify-center gap-10 sm:gap-20">
-                <a href="https://instagram.com/devarph" class="flex items-center space-x-3 text-2xl">
-                    <i class="fa-brands fa-square-instagram text-2xl"></i>
+                <a href="https://instagram.com/devarph"
+                    class="group flex items-center space-x-3 text-2xl transition-transform duration-300 hover:scale-105 hover:text-pink-600">
+                    <i
+                        class="fa-brands fa-square-instagram text-2xl transition-colors duration-300 group-hover:text-pink-600"></i>
                     <span>Instagram</span>
                 </a>
-                <a href="https://www.linkedin.com/in/devarizaph/" class="flex items-center space-x-3 text-2xl">
-                    <i class="fa-brands fa-linkedin text-2xl"></i>
-                    <span>Linkedin</span>
+                <a href="https://www.linkedin.com/in/devarizaph/"
+                    class="group flex items-center space-x-3 text-2xl transition-transform duration-300 hover:scale-105 hover:text-blue-600">
+                    <i
+                        class="fa-brands fa-linkedin text-2xl transition-colors duration-300 group-hover:text-blue-600"></i>
+                    <span>LinkedIn</span>
                 </a>
-                <a href="https://github.com/heradph" class="flex items-center space-x-3 text-2xl">
-                    <i class="fa-brands fa-square-github text-2xl"></i>
-                    <span>Github</span>
+                <a href="https://github.com/heradph"
+                    class="group flex items-center space-x-3 text-2xl transition-transform duration-300 hover:scale-105 hover:text-gray-800">
+                    <i
+                        class="fa-brands fa-square-github text-2xl transition-colors duration-300 group-hover:text-gray-800"></i>
+                    <span>GitHub</span>
                 </a>
-                <a href="https://x.com/nepbk" class="flex items-center space-x-3 text-2xl">
-                    <i class="fa-brands fa-x-twitter text-2xl"></i>
+                <a href="https://x.com/nepbk"
+                    class="group flex items-center space-x-3 text-2xl transition-transform duration-300 hover:scale-105 hover:text-black">
+                    <i
+                        class="fa-brands fa-x-twitter text-2xl transition-colors duration-300 group-hover:text-black"></i>
                     <span>X</span>
                 </a>
-                <a href="https://api.whatsapp.com/send/?phone=6281584342505&text&type=phone_number&app_absent=0&wame_ctl=1" class="flex items-center space-x-3 text-2xl">
-                    <i class="fa-brands fa-whatsapp text-2xl"></i>
-                    <span>Whatsapp</span>
+                <a href="https://api.whatsapp.com/send/?phone=6281584342505&text&type=phone_number&app_absent=0&wame_ctl=1"
+                    class="group flex items-center space-x-3 text-2xl transition-transform duration-300 hover:scale-105 hover:text-green-600">
+                    <i
+                        class="fa-brands fa-whatsapp text-2xl transition-colors duration-300 group-hover:text-green-600"></i>
+                    <span>WhatsApp</span>
                 </a>
-                
-
-                <div class="flex space-x-3 items-center text-xl text-center">
-                    <i class="fa-solid fa-envelope text-2xl"></i>
+                <div
+                    class="group flex space-x-3 items-center text-2xl transition-transform duration-300 hover:scale-105 hover:text-red-600">
+                    <i
+                        class="fa-solid fa-envelope text-2xl transition-colors duration-300 group-hover:text-red-600"></i>
                     <a href="mailto:devarizaph@gmail.com">Email</a>
                 </div>
             </div>
-        </div>  
-    
-        
-    </div>
-    
-            
-
-            {{-- <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform" width="158" height="48">
-            <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple" width="158" height="48">
-            <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal" width="158" height="48">
-            <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic" width="158" height="48"> --}}
-          </div>
         </div>
-      </div>
-      
+    </div>
 </x-layout>
-   
-    
-    
