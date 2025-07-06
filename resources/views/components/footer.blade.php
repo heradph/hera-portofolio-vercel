12af7f3bbdedece723cfbb70dccb81b3ff23a198
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-06 12:30:08 +0700

    adjusments

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,112 +1,100 @@
 <footer class="bg-[#1D503A]">
     <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <div class="lg:flex lg:items-start lg:gap-8">
-        <div class="text-white">
-            <i class="fa-solid fa-hands text-4xl"></i>
+        <div class="lg:flex lg:items-start lg:gap-8">
+            <div class="text-white">
+                <i class="fa-solid fa-hands text-4xl"></i>
 
-        </div>
-  
-        <div class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
-          <div class="col-span-2">
-            <div>
-              <h2 class="text-2xl font-bold text-white">Feel free to contact me!</h2>
-  
-              <p class="mt-4 text-gray-300">
-                Highly open to job opportunities, especially in technology, project management and digital innovation.
-              </p>
             </div>
-          </div>
-  
-          <div class="-mb-13 col-span-2 lg:col-span-3 lg:flex lg:items-end">             
-              <div class="text-gray-300 justify-end w-full focus-within:ring-3 sm:flex sm:items-center sm:gap-4">
-                 devarizaph@gmail.com
-              </div>
-          </div>
-  
-          
-  
-          <ul class="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end">
-            <li>
-                <a
-                  href="https://instagram.com/devarph"
-                  rel="noreferrer"x`
-                  target="_blank"
-                  class=" transition hover:opacity-75"
-                >
-                <i class="fa-brands fa-instagram text-white"></i>
-                </a>
-              </li>
-    
-            <li>
-              <a
-                href="https://www.linkedin.com/in/devarizaph/"
-                rel="noreferrer"
-                target="_blank"
-                class="transition hover:opacity-75"
-              >
-              <i class="fa-brands fa-linkedin text-white"></i>
-              </a>
-            </li>
-            
-            <li>
-                <a
-                    href="https://github.com/heradph"
-                    rel="noreferrer"    
-                    target="_blank"
-                    class="transition hover:opacity-75">
-                <i class="fab fa-github text-white"></i>
-                </a>
-            </li>
-           
-            <li>
-              <a
-                href="https://x.com/nepbk"
-                rel="noreferrer"
-                target="_blank"
-                class="transition hover:opacity-75"
-              >
-              <i class="fa-brands fa-x-twitter text-white"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://api.whatsapp.com/send/?phone=6281584342505&text&type=phone_number&app_absent=0&wame_ctl=1"
-                rel="noreferrer"
-                target="_blank"
-                class="transition hover:opacity-75"
-              >
-              <i class="fa-brands fa-whatsapp text-white"></i>
-              </a>
-            </li>
-            <li>
-              <a href="mailto:devarizaph@gmail.com">
-                <i class="fa-solid fa-envelope text-white "></i>
-              </a>
-            </li>
-          </ul>
+
+            <div class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
+                <div class="col-span-2">
+                    <div>
+                        <h2 class="text-2xl font-bold text-white">Feel free to contact me!</h2>
+
+                        <p class="mt-4 text-gray-300">
+                            Highly open to job opportunities, especially in technology, project management and digital
+                            innovation.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="-mb-13 col-span-2 lg:col-span-3 lg:flex lg:items-end">
+                    <div class="text-gray-300 justify-end w-full focus-within:ring-3 sm:flex sm:items-center sm:gap-4">
+                        devarizaph@gmail.com
+                    </div>
+                </div>
+
+
+
+                <ul class="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end">
+                    <li>
+                        <a href="https://instagram.com/devarph" rel="noreferrer" target="_blank"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fa-brands fa-instagram text-white text-2xl transition-colors duration-300 group-hover:text-pink-500"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/devarizaph/" rel="noreferrer" target="_blank"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fa-brands fa-linkedin text-white text-2xl transition-colors duration-300 group-hover:text-blue-500"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/heradph" rel="noreferrer" target="_blank"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fab fa-github text-white text-2xl transition-colors duration-300 group-hover:text-gray-400"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://x.com/nepbk" rel="noreferrer" target="_blank"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fa-brands fa-x-twitter text-white text-2xl transition-colors duration-300 group-hover:text-black"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://api.whatsapp.com/send/?phone=6281584342505&text&type=phone_number&app_absent=0&wame_ctl=1"
+                            rel="noreferrer" target="_blank"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fa-brands fa-whatsapp text-white text-2xl transition-colors duration-300 group-hover:text-green-400"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:devarizaph@gmail.com"
+                            class="group flex items-center transition-transform duration-300 hover:scale-105">
+                            <i
+                                class="fa-solid fa-envelope text-white text-2xl transition-colors duration-300 group-hover:text-red-400"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
-  
-      <div class="mt-8 border-t border-gray-100 pt-8">
-        <div class="sm:flex sm:justify-between">
-          <p class="text-xs text-gray-400">&copy; 2025. Herawan. All rights reserved.</p>
-  
-          <ul class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
-            <li>
-              <a href="#" class="text-gray-400 transition hover:opacity-75"> Terms & Conditions </a>
-            </li>
-  
-            <li>
-              <a href="#" class="text-gray-400 transition hover:opacity-75"> Privacy Policy </a>
-            </li>
-  
-            <li>
-              <a href="#" class="text-gray-400 transition hover:opacity-75"> Cookies </a>
-            </li>
-          </ul>
+
+        <div class="mt-8 border-t border-gray-100 pt-8">
+            <div class="sm:flex sm:justify-between">
+                <p class="text-xs text-gray-400">&copy; 2025. Herawan. All rights reserved.</p>
+
+                <ul class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
+                    <li>
+                        <a href="#" class="text-gray-400 transition hover:opacity-75"> Terms & Conditions </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="text-gray-400 transition hover:opacity-75"> Privacy Policy </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="text-gray-400 transition hover:opacity-75"> Cookies </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
+</footer>
 </body>
+
 </html>
