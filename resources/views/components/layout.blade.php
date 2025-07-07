d76dfe758f60363e013a042e10f5d4b1db094f4c
Devariza Putera Herawan <devarizaph@gmail.com>
2025-07-07 20:42:11 +0700

    nav-link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,7 @@
     <link rel="shortcut icon" href="/favicon/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
     <link rel="manifest" href="/favicon/site.webmanifest" />
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,5 @@
 @props(['active' => false])
 
-<a {{ $attributes }} class="{{ $active ? "bg-[#FAF5EE] text-black" : "text-gray-300 hover:bg-[#FAF5EE] hover:text-black" }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
+<a {{ $attributes }}
+    class="{{ $active ? 'bg-[#FAF5EE] text-black' : 'text-gray-300 hover:bg-[#FAF5EE] hover:text-black' }} block w-full rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
